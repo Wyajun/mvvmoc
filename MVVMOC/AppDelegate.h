31c0bbbe0cf56b979c909310d2a14a317baa9f05
wangyajun <yajunst@163.com>
2016-06-07 10:08:58 +0800

    Initial Commit

---
+++ MVVMOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVMOC
+//
+//  Created by 王亚军 on 16/6/7.
+//  Copyright © 2016年 王亚军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVVMOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVMOC
+//
+//  Created by 王亚军 on 16/6/7.
+//  Copyright © 2016年 王亚军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
